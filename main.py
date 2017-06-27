27b0e8478a2341bcaeded80215db46c50a846997
Jonathan Muckell <JonMuckell@gmail.com>
2017-06-27 16:23:24 -0400

    edited default input files

--- main.py
+++ main.py
@@ -64,47 +64,12 @@ resultsFileHandler = open('results.csv', 'a')
 # start, lift, transfer, drop, post, end
 #------------------------------------
 
-#run("0_Bed_to_ShowerChair_M.mvnx",  [0, 1480, 1600, 2000, 2200, 2600])
-run("0_Bed_to_ShowerChair_N.mvnx", [0, 1150, 3041, 3450, 4100, 7500] )
+run("0_Bed_to_ShowerChair_M.mvnx",  [0, 1480, 1600, 2000, 2200, 2600])
+#run("0_Bed_to_ShowerChair_N.mvnx", [0, 1150, 3041, 3450, 4100, 7500] )
+#run("0_ShowerChair_to_WheelChair_M.mvnx", [0, 2000, 2550, 2700, 2850, 5000] )
+#run("0_ShowerChair_to_WheelChair_N.mvnx", [0, 1550, 1820, 2400, 2800,  7000 ])
 
-'''
-run("0_ShowerChair_to_WheelChair_M.mvnx", [0, 2000, 2550, 2700, 2850, 5000] )
-run("0_ShowerChair_to_WheelChair_N.mvnx", [0, 1550, 1820, 2400, 2800,  7000 ])
 
-run("1_Bed_to_showerChair_M.mvnx", [0, 900, 1050, 1200, 1380, 1500])
-run("1_Bed_to_showerChair_N.mvnx", [0, 1050, 1320, 1400, 1650, 1800 ])
-run("1_ShowerChair_to_WheelChair_M.mvnx",[0, 650, 800, 1450, 1700, 2000])
-run("1_ShowerChair_to_WheelChair_N.mvnx", [0, 630, 920, 1000, 1400, 1550 ])
-
-run("2_bed_to_ShowerChair_M.mvnx",[0, 1800, 2200, 2325, 2800, 2900 ])
-run("2_Bed_to_ShowerChair_N.mvnx",[0, 1550, 1790, 1950, 2350, 2500] )
-run("2_showerChair_to_wheelChair_M.mvnx", [0, 800, 1070, 1300, 1700, 2500 ])
-run("2_ShowerChair_to_WheelChair_N.mvnx", [2700, 4100, 4370, 4500, 5400, 5500])
-
-run("3_bed_to_chair_M.mvnx", [0, 950, 1100, 1300, 1700, 1750])
-
-run("3_bed_to_showerChair_N.mvnx", [0, 1450, 2050, 2400, 2600, 3000])  
-
-run("3_WheelChair_to_Chair_M.mvnx", [0, 250, 550, 1150, 1470, 1500])
-
-run("4_Bed_to_shower_N.mvnx", [2000, 2600, 2900, 3000, 3500, 3750] )
-run("4_Bed_to_showerChair_M.mvnx", [0, 2700, 3000, 3100, 3300, 3500])
-
-run("5_Bed_to_ShowerChair_M.mvnx", [0, 400, 600, 650, 1050, 1100])
-run("5_bed_to_ShowerChair_N.mvnx", [6300, 7600, 8000, 8300, 8500, 8650])
-run("5_ShowerChair_to_WheelChair_M.mvnx",[0, 450, 700, 800, 950, 1100])
-run("5_ShowerChair_to_WheelChair_N.mvnx", [0, 2400, 2700, 3850, 4150, 4350])
-
-run("6_Bed_to_ShowerChair_M.mvnx", [0, 1050, 1230, 1320, 1450, 1600])
-run("6_bed_to_showerChair_N.mvnx", [0, 3000, 3470, 3500, 3750, 3900])
-run("6_ShowerChair_to_WheelChair_M.mvnx", [0, 320, 450, 470, 735, 1200])
-run("6_ShowerChair_to_WheelChair_N.mvnx", [0, 1400, 1480, 1550, 1900, 3300])
-
-run("7_Bed_to_showerchair_M.mvnx", [0, 1130, 1265, 1350, 1560, 1600])
-run("7_bed_to_showerChair_N.mvnx",[0, 1400, 1500, 1700, 2000, 2150])
-run("7_showerChair_to_wheelchair_M.mvnx", [0, 400, 520, 710, 1000, 2100])
-run("7_showerChair_to_Wheelchair_N.mvnx", [0, 1000, 1125, 1300, 1560, 1800])
-'''
 #------------------------------------
 resultsFileHandler.close()
 print("CODE FINISHED!")
